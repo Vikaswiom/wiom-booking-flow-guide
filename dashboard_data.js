5ed884a5593ec8c5039185e945f0183e8d338d55
GitHub Action <action@github.com>
2026-06-19 07:41:17 +0000

    Auto-refresh Jun 19 07:41

--- dashboard_data.js
+++ dashboard_data.js
@@ -1,60 +1,60 @@
 const DASHBOARD_DATA = {
-  "refreshed_at": "2026-06-18 07:17 UTC",
-  "post_end_date": "2026-06-13",
+  "refreshed_at": "2026-06-19 07:41 UTC",
+  "post_end_date": "2026-06-14",
   "post_funnel": {
-    "total": 6962,
-    "ssid": 6799,
-    "address": 6755,
-    "verified": 6537,
-    "notif": 6123,
-    "interested": 5183,
-    "slot": 5068,
-    "confirmed": 5022,
-    "assigned": 4505,
-    "otp": 2307,
-    "cancelled": 4362
+    "total": 7094,
+    "ssid": 6928,
+    "address": 6884,
+    "verified": 6665,
+    "notif": 6230,
+    "interested": 5260,
+    "slot": 5141,
+    "confirmed": 5095,
+    "assigned": 4566,
+    "otp": 2338,
+    "cancelled": 4454
   },
   "new_release": {
-    "installs": 88305,
-    "homepage": 123095,
-    "check": 104822,
-    "serviceable": 65335,
-    "unserviceable": 4216,
-    "how_works": 63184,
-    "get_started": 19574,
-    "cost": 17843,
-    "pay100": 12068,
+    "installs": 90285,
+    "homepage": 125417,
+    "check": 106889,
+    "serviceable": 66514,
+    "unserviceable": 4277,
+    "how_works": 64326,
+    "get_started": 20232,
+    "cost": 18451,
+    "pay100": 12464,
     "location": 0,
-    "paid": 2602
+    "paid": 2692
   },
   "variants": {
     "A": {
-      "entry": 5111,
-      "cost": 4693,
-      "pay100": 3404,
+      "entry": 5117,
+      "cost": 4697,
+      "pay100": 3407,
       "location": 0,
-      "paid": 703
+      "paid": 706
     },
     "B": {
-      "entry": 4451,
-      "cost": 4123,
-      "pay100": 2859,
+      "entry": 4456,
+      "cost": 4127,
+      "pay100": 2864,
       "location": 0,
-      "paid": 600
+      "paid": 601
     },
     "C": {
-      "entry": 5118,
-      "cost": 4494,
-      "pay100": 3141,
+      "entry": 5453,
+      "cost": 4797,
+      "pay100": 3366,
       "location": 0,
-      "paid": 664
+      "paid": 714
     },
     "D": {
-      "entry": 4894,
-      "cost": 4533,
-      "pay100": 2664,
+      "entry": 5206,
+      "cost": 4830,
+      "pay100": 2827,
       "location": 0,
-      "paid": 635
+      "paid": 671
     }
   }
 };
