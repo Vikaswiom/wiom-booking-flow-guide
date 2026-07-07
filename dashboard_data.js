b23f9722e2f2c6b019d3fefd28edc6eec6e83c10
GitHub Action <action@github.com>
2026-07-07 06:31:20 +0000

    Auto-refresh Jul 07 06:31

--- dashboard_data.js
+++ dashboard_data.js
@@ -1,60 +1,60 @@
 const DASHBOARD_DATA = {
-  "refreshed_at": "2026-07-06 06:54 UTC",
-  "post_end_date": "2026-07-01",
+  "refreshed_at": "2026-07-07 06:31 UTC",
+  "post_end_date": "2026-07-02",
   "post_funnel": {
-    "total": 8876,
-    "ssid": 8679,
-    "address": 8608,
-    "verified": 8384,
+    "total": 8995,
+    "ssid": 8798,
+    "address": 8726,
+    "verified": 8502,
     "notif": 6537,
     "interested": 5484,
     "slot": 5352,
     "confirmed": 5306,
     "assigned": 4717,
     "otp": 2389,
-    "cancelled": 5845
+    "cancelled": 5912
   },
   "new_release": {
-    "installs": 129127,
-    "homepage": 169421,
-    "check": 147466,
-    "serviceable": 87136,
-    "unserviceable": 5186,
-    "how_works": 84386,
-    "get_started": 25815,
-    "cost": 23421,
-    "pay100": 15488,
+    "installs": 131639,
+    "homepage": 172228,
+    "check": 150077,
+    "serviceable": 88439,
+    "unserviceable": 5250,
+    "how_works": 85654,
+    "get_started": 26327,
+    "cost": 23896,
+    "pay100": 15742,
     "location": 0,
-    "paid": 3362
+    "paid": 3414
   },
   "variants": {
     "A": {
-      "entry": 5266,
-      "cost": 4768,
-      "pay100": 3460,
+      "entry": 5279,
+      "cost": 4769,
+      "pay100": 3461,
       "location": 0,
-      "paid": 730
+      "paid": 731
     },
     "B": {
-      "entry": 4582,
+      "entry": 4586,
       "cost": 4187,
-      "pay100": 2920,
+      "pay100": 2922,
       "location": 0,
       "paid": 620
     },
     "C": {
-      "entry": 7415,
-      "cost": 6514,
-      "pay100": 4544,
+      "entry": 7433,
+      "cost": 6530,
+      "pay100": 4556,
       "location": 0,
-      "paid": 931
+      "paid": 934
     },
     "D": {
-      "entry": 8552,
-      "cost": 7952,
-      "pay100": 4564,
+      "entry": 9029,
+      "cost": 8410,
+      "pay100": 4803,
       "location": 0,
-      "paid": 1081
+      "paid": 1129
     }
   }
 };
