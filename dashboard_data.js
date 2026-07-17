73057b1912133cec70545885f716c9e84425dd07
GitHub Action <action@github.com>
2026-07-17 05:28:57 +0000

    Auto-refresh Jul 17 05:28

--- dashboard_data.js
+++ dashboard_data.js
@@ -1,58 +1,58 @@
 const DASHBOARD_DATA = {
-  "refreshed_at": "2026-07-16 05:27 UTC",
-  "post_end_date": "2026-07-11",
+  "refreshed_at": "2026-07-17 05:28 UTC",
+  "post_end_date": "2026-07-12",
   "post_funnel": {
-    "total": 10038,
-    "ssid": 9819,
-    "address": 9738,
-    "verified": 9510,
+    "total": 10139,
+    "ssid": 9919,
+    "address": 9836,
+    "verified": 9608,
     "notif": 6538,
     "interested": 5485,
     "slot": 5353,
     "confirmed": 5307,
     "assigned": 4717,
     "otp": 2389,
-    "cancelled": 6644
+    "cancelled": 6739
   },
   "new_release": {
-    "installs": 156314,
-    "homepage": 199699,
-    "check": 175566,
-    "serviceable": 101039,
-    "unserviceable": 5756,
-    "how_works": 97906,
-    "get_started": 28917,
-    "cost": 26186,
-    "pay100": 17079,
+    "installs": 158954,
+    "homepage": 202643,
+    "check": 178293,
+    "serviceable": 102522,
+    "unserviceable": 5828,
+    "how_works": 99355,
+    "get_started": 28992,
+    "cost": 26245,
+    "pay100": 17119,
     "location": 0,
     "paid": 3751
   },
   "variants": {
     "A": {
-      "entry": 5363,
-      "cost": 4774,
-      "pay100": 3469,
+      "entry": 5369,
+      "cost": 4775,
+      "pay100": 3470,
       "location": 0,
       "paid": 734
     },
     "B": {
-      "entry": 4652,
+      "entry": 4657,
       "cost": 4191,
       "pay100": 2931,
       "location": 0,
       "paid": 626
     },
     "C": {
-      "entry": 7614,
-      "cost": 6678,
-      "pay100": 4668,
+      "entry": 7636,
+      "cost": 6698,
+      "pay100": 4682,
       "location": 0,
       "paid": 960
     },
     "D": {
-      "entry": 11288,
-      "cost": 10543,
-      "pay100": 6011,
+      "entry": 11330,
+      "cost": 10581,
+      "pay100": 6036,
       "location": 0,
       "paid": 1431
     }
