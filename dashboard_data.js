6485ebb49a53ef18a1c831f6599cb90f77a4521a
GitHub Action <action@github.com>
2026-07-23 05:42:42 +0000

    Auto-refresh Jul 23 05:42

--- dashboard_data.js
+++ dashboard_data.js
@@ -1,60 +1,60 @@
 const DASHBOARD_DATA = {
-  "refreshed_at": "2026-07-22 05:37 UTC",
-  "post_end_date": "2026-07-17",
+  "refreshed_at": "2026-07-23 05:42 UTC",
+  "post_end_date": "2026-07-18",
   "post_funnel": {
-    "total": 10886,
-    "ssid": 10649,
-    "address": 10551,
-    "verified": 10313,
+    "total": 11073,
+    "ssid": 10833,
+    "address": 10736,
+    "verified": 10496,
     "notif": 6538,
     "interested": 5485,
     "slot": 5353,
     "confirmed": 5307,
     "assigned": 4717,
     "otp": 2389,
-    "cancelled": 7134
+    "cancelled": 7339
   },
   "new_release": {
-    "installs": 171331,
-    "homepage": 216320,
-    "check": 190980,
-    "serviceable": 109287,
-    "unserviceable": 6116,
-    "how_works": 105851,
-    "get_started": 29261,
-    "cost": 26414,
-    "pay100": 17246,
+    "installs": 173494,
+    "homepage": 218739,
+    "check": 193223,
+    "serviceable": 110488,
+    "unserviceable": 6188,
+    "how_works": 106932,
+    "get_started": 29300,
+    "cost": 26439,
+    "pay100": 17270,
     "location": 0,
-    "paid": 3778
+    "paid": 3785
   },
   "variants": {
     "A": {
-      "entry": 5407,
+      "entry": 5408,
       "cost": 4777,
-      "pay100": 3473,
+      "pay100": 3474,
       "location": 0,
       "paid": 735
     },
     "B": {
-      "entry": 4688,
+      "entry": 4695,
       "cost": 4192,
       "pay100": 2933,
       "location": 0,
       "paid": 627
     },
     "C": {
-      "entry": 7707,
-      "cost": 6751,
-      "pay100": 4729,
+      "entry": 7722,
+      "cost": 6761,
+      "pay100": 4736,
       "location": 0,
-      "paid": 969
+      "paid": 971
     },
     "D": {
-      "entry": 11459,
-      "cost": 10694,
-      "pay100": 6111,
+      "entry": 11475,
+      "cost": 10709,
+      "pay100": 6127,
       "location": 0,
-      "paid": 1447
+      "paid": 1452
     }
   }
 };
