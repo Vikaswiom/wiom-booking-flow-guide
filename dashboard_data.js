7333ce5e34a92f3c7bcbd5dbe632ef64d591bc54
GitHub Action <action@github.com>
2026-08-01 05:50:23 +0000

    Auto-refresh Aug 01 05:50

--- dashboard_data.js
+++ dashboard_data.js
@@ -1,60 +1,60 @@
 const DASHBOARD_DATA = {
-  "refreshed_at": "2026-07-31 05:55 UTC",
-  "post_end_date": "2026-07-26",
+  "refreshed_at": "2026-08-01 05:50 UTC",
+  "post_end_date": "2026-07-27",
   "post_funnel": {
-    "total": 12643,
-    "ssid": 12274,
-    "address": 12240,
-    "verified": 11996,
+    "total": 12882,
+    "ssid": 12411,
+    "address": 12467,
+    "verified": 12221,
     "notif": 6540,
     "interested": 5487,
     "slot": 5355,
     "confirmed": 5309,
     "assigned": 4719,
     "otp": 2389,
-    "cancelled": 8315
+    "cancelled": 8433
   },
   "new_release": {
-    "installs": 191818,
-    "homepage": 239054,
-    "check": 211978,
-    "serviceable": 120415,
-    "unserviceable": 6791,
-    "how_works": 115849,
-    "get_started": 29596,
-    "cost": 26584,
-    "pay100": 17392,
+    "installs": 194725,
+    "homepage": 242201,
+    "check": 214894,
+    "serviceable": 122039,
+    "unserviceable": 6903,
+    "how_works": 117313,
+    "get_started": 29645,
+    "cost": 26601,
+    "pay100": 17405,
     "location": 0,
-    "paid": 3832
+    "paid": 3835
   },
   "variants": {
     "A": {
-      "entry": 5469,
+      "entry": 5478,
       "cost": 4778,
-      "pay100": 3476,
+      "pay100": 3477,
       "location": 0,
       "paid": 738
     },
     "B": {
-      "entry": 4739,
+      "entry": 4755,
       "cost": 4192,
       "pay100": 2936,
       "location": 0,
-      "paid": 631
+      "paid": 632
     },
     "C": {
-      "entry": 7818,
-      "cost": 6830,
-      "pay100": 4795,
+      "entry": 7827,
+      "cost": 6837,
+      "pay100": 4801,
       "location": 0,
-      "paid": 989
+      "paid": 990
     },
     "D": {
-      "entry": 11570,
-      "cost": 10784,
-      "pay100": 6185,
+      "entry": 11585,
+      "cost": 10794,
+      "pay100": 6191,
       "location": 0,
-      "paid": 1474
+      "paid": 1475
     }
   }
 };
